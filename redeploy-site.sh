49aee46dc7dbe2d316c45a7bd60581c31101e35c
ispilledmymilk <86566762+ispilledmymilk@users.noreply.github.com>
2026-07-23 15:20:05 -0400

    Add Docker Compose setup and simplify redeploy script.

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,10 @@
-#!/bin/bash
+s#!/bin/bash
 set -e
 
-# 1. cd into project folder
 cd "$HOME/saiborg-personal-website"
 
-# 2. Sync VPS repo with latest main from GitHub
 git fetch
 git reset origin/main --hard
 
-# 3. Enter virtual environment and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-
-# 4. Restart myportfolio systemd service
-systemctl restart myportfolio
-systemctl --no-pager --full status myportfolio | head -15
-
-echo "==> Redeploy complete"
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
